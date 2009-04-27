2406bfa1fc535c75ef75996aced38ae87f2d6c8a
Enrico Tröger <enrico.troeger@uvena.de>
2009-04-27 19:31:21 +0000

    [Ruby] Properly eat string closing character

--- Test/bug1742588.rb
+++ Test/bug1742588.rb
@@ -1,7 +1,33 @@
+#
+# ctags -f - bug1742588.rb should output:
+#
+# classes:
+#   A
+#   B
+#
+# methods
+#   A.a
+#   A.b
+#   B.b_a
+#   B.b_c
+#
+
 class A
  def a()
   super(" do ")
  end
  def b()
  end
+end
+
+class B
+  def b_a
+    File.open("foo", "r") do |infile|
+      infile.readline
+    end
+  end
+
+  def b_c
+    print "bar"
+  end
 end
