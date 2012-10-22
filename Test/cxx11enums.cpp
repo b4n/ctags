df9d284365c06be860128f45130e66a02984a551
Colomban Wendling <ban@herbesfolles.org>
2012-10-22 22:04:11 +0200

    Parse C++11 classed enums

--- Test/cxx11enums.cpp
+++ Test/cxx11enums.cpp
@@ -9,6 +9,22 @@
  * E1::C	enumerator
  * E2		enum
  * E2::A	enumerator
+ * E3		enum
+ * E3::A	enumerator
+ * E3::B	enumerator
+ * E3::C	enumerator
+ * E4		enum
+ * E4::A	enumerator
+ * E4::B	enumerator
+ * E4::C	enumerator
+ * E5		enum
+ * E5::A	enumerator
+ * E5::B	enumerator
+ * E5::C	enumerator
+ * E6		enum
+ * E6::A	enumerator
+ * E6::B	enumerator
+ * E6::C	enumerator
  */
 
 enum E0 {
@@ -23,4 +39,20 @@ enum E1 : unsigned int {
 
 enum E2 : int {
 	A
+};
+
+enum class E3 {
+	A, B, C
+};
+
+enum class E4 : long {
+	A, B, C
+};
+
+enum struct E5 {
+	A, B, C
+};
+
+enum struct E6 : unsigned long int {
+	A, B, C
 };
